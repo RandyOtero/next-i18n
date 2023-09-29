036ffdf186651090f30711f5618313ad31eb9e7f
Randy Otero <randy@placeshaker.fr>
2023-09-29 13:02:52 +0100

    Update next.config.js

--- nextjs-blog/next.config.js
+++ nextjs-blog/next.config.js
@@ -1,5 +1,45 @@
 const { i18n } = require("./next-i18next.config");
 
 module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/locales/:all*(json)',
+        locale: false,
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=0, must-revalidate',
+          },
+        ],
+      },
+      {
+        source: '/service-worker.js',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=0, must-revalidate',
+          },
+        ],
+      },
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+        ],
+      },
+    ];
+  },
   i18n,
 };

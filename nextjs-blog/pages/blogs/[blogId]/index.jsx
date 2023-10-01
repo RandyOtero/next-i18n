5939b450fb90a2f0ab3cbe016a0b6055c02c508d
Randy Otero <randy@placeshaker.fr>
2023-10-01 21:01:41 +0100

    Update references

--- nextjs-blog/pages/blogs/[blogId]/index.jsx
+++ nextjs-blog/pages/blogs/[blogId]/index.jsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import styles from "../../../styles/Home.module.css";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useTranslation } from "next-i18next";
 import dynamic from "next/dynamic";
